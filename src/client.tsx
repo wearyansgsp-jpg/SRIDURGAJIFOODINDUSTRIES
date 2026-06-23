f4d338716c3669f221630bda3ed0bff31738aaf6
wearyansgsp-jpg <wearyansgsp@gmail.com>
2026-06-23 23:53:01 +0530

    Update client.tsx

--- src/client.tsx
+++ src/client.tsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider, createRouter } from '@tanstack/react-router'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { routeTree } from './routeTree.gen'
 import './styles.css'
 
-const router = createRouter({ routeTree })
+const queryClient = new QueryClient()
+
+const router = createRouter({
+  routeTree,
+  defaultPreload: 'intent',
+  context: {
+    queryClient,
+  },
+})
 
 declare module '@tanstack/react-router' {
   interface Register {
@@ -12,8 +21,12 @@ declare module '@tanstack/react-router' {
   }
 }
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const rootElement = document.getElementById('root')!
+
+ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </React.StrictMode>
 )
